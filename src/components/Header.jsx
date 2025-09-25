a5f363f0e08b8f44852d3d3a282534793335ef6a
juanluengo88 <jluengo@frba.utn.edu.ar>
2025-09-25 12:19:56 -0300

    añado dokerfile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Page_Header() {
             alt="Pokeball" 
             className="pokeball-icon" 
           />
-          <h1>Pokemon Finder</h1>
+          <h1>Pokemon Findr</h1>
           <img 
             src="https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg" 
             alt="Pokeball" 
